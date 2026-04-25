049ed10292a29f6757e77f528f0f54ea56fff90c
Samuel Murillo <contact@samuelmurillo.xyz>
2026-04-25 20:09:23 +0200

    Add scb

---
+++ dot_bin/executable_scopebuddy-post.sh
@@ -0,0 +1,5 @@
+power_integer=`cat $HOME/.config/scopebuddy/scopebuddy-razer-power-integer.txt`
+fan_rpm=`cat $HOME/.config/scopebuddy/scopebuddy-razer-fan-rpm.txt`
+
+razer-cli write power ac "$power_integer"
+razer-cli write fan ac "$fan_rpm"
